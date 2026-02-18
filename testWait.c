21746b1040ce8d9ba136fd1b8ee55c918ebb5e4f
shixuzhimeng <1516446316@qq.com>
2026-02-18 23:55:59 +0800

    wait/waitpid

---
+++ testWait.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main () {
+    pid_t id = fork();
+    if(id < 0) {
+        perror("fork");
+        return 1;
+    }
+    else if(id == 0) {
+        int cnt = 5;
+        while(cnt) {
+            printf("I am child, pid:%d, ppid:%d, cnt:%d\n", getpid(), getppid(), cnt);
+            cnt--;
+            sleep(1);
+        }
+        exit(0);
+    }
+    else {
+        while(1) {
+            printf("I am father, pid:%d, ppid:%d, cnt:%d\n", getpid(), getppid(), cnt);
+            cnt--;
+            sleep(1);
+        }
+    }
+    int status = 0;
+    pid_t ret = waitpid(id, &status, 0);
+    if(ret == id) {
+        printf("wait success, ret:%d\n",ret);
+    }
+
+    // pid_t ret = wait(NULL);
+    // if(ret == id) {
+    //     printf("wait success, ret: %d\n", ret);
+    // }
+
+    return 0;
+}
