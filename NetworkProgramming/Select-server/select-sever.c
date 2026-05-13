0dd4f9aa7636492ab5e4369ba036266a95d942a7
shixuzhimeng <1516446316@qq.com>
2026-05-13 23:05:09 +0800

    多线程服务器简单实现优化过后的版本

--- NetworkProgramming/Select-server/select-sever.c
+++ NetworkProgramming/Select-server/select-sever.c
@@ -10,10 +10,11 @@
 #define SERV_PORT 9999
 
 int main(int argc, char *argv[]) {
-    int listenfd, connfd;
-    char buf[BUFSIZ];
-    struct sockaddr_in serv_addr, clie_addr;
+    int listenfd, sockfd, connfd, maxi, nready;
+    int client[FD_SETSIZE];
+    char buf[BUFSIZ], str[INET_ADDRSTRLEN];
     socklen_t clie_addr_len;
+    struct sockaddr_in serv_addr, clie_addr;
 
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
@@ -32,42 +33,70 @@ int main(int argc, char *argv[]) {
     fd_set rset, allset;            //定义读集合， 备份集合allset
     int ret, maxfd = 0, n, i, j;
     maxfd = listenfd;               //最大文件描述符
+    maxi = -1;
+    for(i = 0; i < FD_SETSIZE; i++) {
+        client[i] = -1;
+    }
     FD_ZERO(&allset);               //清空监听集合
     FD_SET(listenfd, &allset);      //将待监听的fd添加到监听集合中去
     while(1) {
         rset = allset;              //备份
-        ret = select(maxfd + 1, &rset, NULL, NULL, NULL); 
-        if(ret < 0) {
+        nready = select(maxfd + 1, &rset, NULL, NULL, NULL); 
+        if(nready < 0) {
             perr_exit("select error");
         }
         if(FD_ISSET(listenfd, &rset)) {           // listenfd满足监听的读事件
             clie_addr_len = sizeof(clie_addr);
             connfd = Accept(listenfd, (struct sockaddr *)&clie_addr, &clie_addr_len);    //建立链接 --- 不会阻塞
+            printf("received from %s at PORT %d\n", 
+                inet_ntop(AF_INET, &clie_addr.sin_addr, str, sizeof(str)),
+            ntohs(clie_addr.sin_port));
+            
+            for(i = 0; i < FD_SETSIZE; i++) {
+                if(client[i] < 0) {
+                    client[i] = connfd;
+                    break;
+                }
+            }
+
+            if(i == FD_SETSIZE){
+                fputs("too many clients\n", stderr);
+                exit(1);
+            }
+
             FD_SET(connfd, &allset);              //将新产生的fd添加到监听集合中去，监听数据读事件
             if(maxfd < connfd) {
                 maxfd = connfd;                   // 修改maxfd
             }
-            if(ret == 1) {                        //说明select只返回一个， 并且是listenfd，后续执行无须执行
+            if(i > maxi) {
+                maxi = i;
+            }
+            if(--nready == 0) {                        //说明select只返回一个， 并且是listenfd，后续执行无须执行
                 continue;
             }
         }
         
 
-        for(i = listenfd + 1; i <= maxfd; i++) {      //处理满足读事件的fd
-            if(FD_ISSET(i, &rset)) {                  //找到满足读事件的fd
-                n = Read(i, buf, sizeof(buf));
-                if(n == 0) {                          //检测到客户端关闭链接了
-                    close(i);
-                    FD_CLR(i, &allset);               //将关闭的fd，移除监听集合
+        for(i = 0; i <= maxi; i++) {      //处理满足读事件的fd
+            if((sockfd = client[i]) < 0) {
+                continue;;
+            }
+            if(FD_ISSET(sockfd, &rset)) {                  //找到满足读事件的fd
+                if((n = Read(sockfd, buf, sizeof(buf))) == 0) {
+                    close(sockfd);
+                    FD_CLR(sockfd, &allset);
+                    client[i] = -1;
                 }
-                else if(n == -1){
-                    perr_exit("read error");
+                else if(n > 0) {
+                    for(j = 0; j < n; j++) {
+                        buf[j] = toupper(buf[j]);
+                    }
+                    Write(sockfd, buf, n);
+                    Write(STDOUT_FILENO, buf, n);
                 }
-                for(j = 0; j < n; j++) {
-                    buf[j] = toupper(buf[j]);
+                if(--nready == 0) {
+                    break;
                 }
-                write(i, buf, n);
-                write(STDOUT_FILENO, buf, n);
             }
         }
     }
